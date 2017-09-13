bd32cff1bbf24979cf571eefae1de7782c1012c8
Jason-Delancey <jason.r.delancey@gmail.com>
2017-09-13 13:52:18 -0400

    added puts() statement on test executable and commented out the main.c file

--- OS_I_Class/main.c
+++ OS_I_Class/main.c
@@ -5,11 +5,11 @@
 //  Created by CloudStrife on 9/13/17.
 //  Copyright © 2017 CloudStrife. All rights reserved.
 //
-
+/*
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
     // insert code here....
     printf("Hello, Columbia!\n");
     return 0;
-}
+}*/
