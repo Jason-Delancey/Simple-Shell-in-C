ec668305b48edd5e4d3f224549d2b81a270a3b75
Jason-Delancey <jason.r.delancey@gmail.com>
2017-09-13 07:14:59 -0400

    Initial Commit

---
+++ OS I Class/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  OS I Class
+//
+//  Created by CloudStrife on 9/13/17.
+//  Copyright © 2017 CloudStrife. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
