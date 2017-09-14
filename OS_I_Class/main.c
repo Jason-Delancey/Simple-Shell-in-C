95af6ff91336bc8d20efa76fa6953b908e9d1fdd
Jason-Delancey <jason.r.delancey@gmail.com>
2017-09-14 19:24:22 -0400

    added examples using conditionals

--- OS_I_Class/main.c
+++ OS_I_Class/main.c
@@ -11,31 +11,53 @@
 
 int main(int argc, const char * argv[])
 {
-     
     
-    /* creating random generator
-    int r;
-    srand ((unsigned)time(NULL));
-    r = rand();
-    printf("This is a pseudo random number: %d\n",r);*/
+    /* ouutput values 10-1
+     int x;
+     for(x=10 ; x>0 ; x--)
+     {
+     printf("%2d\n",x);
+     }*/
+    
+    /* ouutput even values 2-20
+     int x=2;
+     while (x<21)
+     {
+     printf("%2d\n",x);
+     x+=2;
+     }*/
+    
+    /* ouutput values 1-20
+     int x;
+     for(x=1 ; x<21 ; x++)
+     {
+     printf("%2d\n",x);
+     }*/
+    
+    /* creating random generator, seeding randomizer
+     int r;
+     srand ((unsigned)time(NULL));
+     r = rand();
+     printf("This is a pseudo random number: %d\n",r);*/
     
     /* using a place holder to output an integer
-    printf("Everyone knows 2+2 = %d\n",2+2);*/
+     printf("Everyone knows 2+2 = %d\n",2+2);*/
     
     /* using an escape character to include brackets inside brackets
-    printf("My name is \"Jason\"\n");*/
+     printf("My name is \"Jason\"\n");*/
     
     /* puts("Just another");
-    puts("...string.");*/
+     puts("...string.");*/
+    
     return(0);
 }
 
 /*
  Default code inserted when creating a new source file
-#include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here....
-    printf("Hello, Columbia!\n");
-    return 0;
-}*/
+ #include <stdio.h>
+ 
+ int main(int argc, const char * argv[]) {
+ // insert code here....
+ printf("Hello, Columbia!\n");
+ return 0;
+ }*/
