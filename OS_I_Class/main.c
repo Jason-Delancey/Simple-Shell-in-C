f45f514f289b1ed112db751bcffe170a7c5409de
Jason Delancey <jason.r.delancey@gmail.com>
2017-09-17 08:48:10 -0400

    updated notes

--- OS_I_Class/main.c
+++ OS_I_Class/main.c
@@ -9,21 +9,31 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define ROWS 5
+#define COLUMNS 10
+
 
 // void evaluate(int aNumber);
 
 int main(int argc, const char * argv[])
 {
-    /* used for testing the protoptype function evaluate()
-    evaluate(10);*/
     
+    
+    /*char sentences[ROWS][COLUMNS] = {"string1", "string2", "string3", "PINOCCHIO"};
+    int x = 0;
+    for(x = 0; x < ROWS; x++)
+        puts(sentences[x]);*/
+    
+    /* used for testing the protoptype function evaluate()
+     evaluate(10);*/
+        
     /* ouutput values 10-1
      int x;
      for(x=10 ; x>0 ; x--)
      {
      printf("%2d\n",x);
      }*/
-    
+        
     /* ouutput even values 2-20
      int x=2;
      while (x<21)
@@ -31,41 +41,41 @@ int main(int argc, const char * argv[])
      printf("%2d\n",x);
      x+=2;
      }*/
-    
+        
     /* ouutput values 1-20
      int x;
      for(x=1 ; x<21 ; x++)
      {
      printf("%2d\n",x);
      }*/
-    
+        
     /* creating random generator, seeding randomizer
      int r;
      srand ((unsigned)time(NULL));
      r = rand();
      printf("This is a pseudo random number: %d\n",r);*/
-    
+        
     /* using a place holder to output an integer
      printf("Everyone knows 2+2 = %d\n",2+2);*/
-    
+        
     /* using an escape character to include brackets inside brackets
      printf("My name is \"Jason\"\n");*/
-    
+        
     /* puts("Just another");
      puts("...string.");*/
-    
-    return(0);
+        
+        return(0);
 }
 
 /*void evaluate(int aNumber)
-{
-    if(aNumber < 10)
-        printf("%d is less than 10\n",aNumber);
-    else if(aNumber > 10)
-        printf("%d is greater than 10\n",aNumber);
-    else
-        printf("%d is equal to 10\n",aNumber);
-}*/
+ {
+ if(aNumber < 10)
+ printf("%d is less than 10\n",aNumber);
+ else if(aNumber > 10)
+ printf("%d is greater than 10\n",aNumber);
+ else
+ printf("%d is equal to 10\n",aNumber);
+ }*/
 
 /*
  Default code inserted when creating a new source file
