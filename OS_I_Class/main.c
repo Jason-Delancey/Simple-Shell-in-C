5d0be25029cb195b0f717dd234be3e16458757f7
Jason-Delancey <jason.r.delancey@gmail.com>
2017-09-20 15:45:42 -0400

    exit for parsing the command input

--- OS_I_Class/main.c
+++ OS_I_Class/main.c
@@ -16,7 +16,7 @@ int main(int argc, const char * argv[])
 {
     char *cmdLine;
     char **args;
-    int RUNNING,childPID;
+    int RUNNING,count;
     
     /* Run continuously and display a prompt when waiting for input. Prompt should be
      EXACTLY $ and nothing else. */
@@ -31,21 +31,46 @@ int main(int argc, const char * argv[])
         fgets(cmdLine, MAX_SIZE, stdin);
         if (cmdLine == NULL)
         {
-            puts("Memory Error");
+            fprintf(stderr, "Memory Error");
             break;
         }
         
         //Split a cmd line into args and store them in the args array
         char *anArg = (char *)malloc(sizeof(char) * MAX_SIZE);
+        args = (char **)malloc(sizeof(anArg) * 6);
+        
+        
         anArg = strtok(cmdLine, " ,.-");
+        count = 0;
         while(anArg != NULL)
         {
             //printf("%s\n",anArg);
+            args[count] = anArg;
+            //printf("%s\n",args[count]);
             anArg = strtok(NULL, " ,.-");
         }
         
-        //Execute the args
+        if(strcmp(args[0], "exit"))
+            exit(0);
+       
         
+        /*Execute the command using the args
+        pid_t pid;
+        int status;
+        
+        pid = fork();
+        if ( pid == 0 )
+        {
+            printf( "This is being printed from the child process\n" );
+            if(execvp(cmdLine[0], cmdLine) == -1)
+                fprintf(stderr, "Error in fork() call");
+        }
+        else
+        {
+            printf( "This is being printed in the parent process:\n"
+                   " - the process identifier (pid) of the child is %d\n", pid );
+        }*/
+
         
         RUNNING = 0;
     };
