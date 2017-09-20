e53dfbfe7a031200ae93b7d54e2e8619d7dce3c0
Jason-Delancey <jason.r.delancey@gmail.com>
2017-09-20 19:20:06 -0400

    add a level of abstract by including prototype functions

--- OS_I_Class/main.c
+++ OS_I_Class/main.c
@@ -12,11 +12,16 @@
 
 #define MAX_SIZE 1024
 
+char *getCommandLine(char *aLine);
+char **parseCommandLine(char **arguments, char *anArgument, char *commandLine);
+void execute(char **args);
+
 int main(int argc, const char * argv[])
 {
-    char *cmdLine;
-    char **args;
-    int RUNNING,count;
+    char *cmdLine = (char *)malloc(sizeof(char) * MAX_SIZE);
+    char *anArg = (char *)malloc(sizeof(char) * MAX_SIZE);
+    char **args = (char **)malloc(sizeof(anArg) * 6);
+    int RUNNING;
     
     /* Run continuously and display a prompt when waiting for input. Prompt should be
      EXACTLY $ and nothing else. */
@@ -25,64 +30,71 @@ int main(int argc, const char * argv[])
     {
         //Print the prompt
         printf("$");
-        
-        //Read a command line from stdin
-        cmdLine = (char *)malloc(sizeof(char) * MAX_SIZE);
-        fgets(cmdLine, MAX_SIZE, stdin);
-        if (cmdLine == NULL)
-        {
-            fprintf(stderr, "Memory Error");
-            break;
-        }
-        
-        //Split a cmd line into args and store them in the args array
-        char *anArg = (char *)malloc(sizeof(char) * MAX_SIZE);
-        args = (char **)malloc(sizeof(anArg) * 6);
-        
-        
-        anArg = strtok(cmdLine, " ,.-");
-        count = 0;
-        while(anArg != NULL)
-        {
-            //printf("%s\n",anArg);
-            args[count] = anArg;
-            //printf("%s\n",args[count]);
-            anArg = strtok(NULL, " ,.-");
-        }
+        cmdLine = getCommandLine(cmdLine);
+        args = parseCommandLine(args, anArg, cmdLine);
         
         if(strcmp(args[0], "exit"))
             exit(0);
+        execute(args);
         /*else
          puts(args[1]);*/
-        
-        //Execute the command using the args
-        pid_t  pid;
-        int    status;
-        
-        if ((pid = fork()) < 0) // fork a child process
-        {
-            fprintf(stderr, "***** ERROR: forking the child process failed\n");
-            exit(1);
-        }
-        else if (pid == 0) // let the child process execute
-        {
-            // execute the command unless it fails and returns -1
-            if (execvp(*args, args) < 0)
-            {
-                printf("***** ERROR: the exec process failed\n");
-                exit(1);
-            }
-        }
-        else // for the parent:
-        {
-            // wait for completion of child process
-            while (wait(&status) != pid)
-                ;
-        }
         RUNNING = 0;
     };
     
     
     return(0);
+}
+
+char *getCommandLine(char *aLine)
+{
+    //Read a command line from stdin and store it
+    fgets(aLine, MAX_SIZE, stdin);
+    if (aLine == NULL)
+        fprintf(stderr, "error: %s\n", "***** ERROR: Memory Error\n");
+    
+    return aLine;
+}
+
+char **parseCommandLine(char **args, char *anArg, char *cmdLine)
+{
+    //Split a cmd line into args and store them in the args array
+    anArg = strtok(cmdLine, " ,.-");
+    int count = 0;
+    while(anArg != NULL)
+    {
+        //printf("%s\n",anArg);
+        args[count] = anArg;
+        //printf("%s\n",args[count]);
+        anArg = strtok(NULL, " ,.-");
+    }
+    return args;
+}
+
+void execute(char **args)
+{
+    //Execute the command using the args
+    pid_t  pid;
+    int    status;
+    
+    if ((pid = fork()) < 0) // fork a child process
+    {
+        fprintf(stderr, "error: %s\n", "***** ERROR: forking the child process failed\n");
+        exit(1);
+    }
+    else if (pid == 0) // let the child process execute
+    {
+        // execute the command in the child process
+        if (execvp(*args, args) < 0)
+        {
+            fprintf(stderr, "error: %s\n", "***** ERROR: the execution process failed\n");
+            exit(1);
+        }
+    }
+    else // for the parent:
+    {
+        // wait for completion of child process
+        while (wait(&status) != pid)
+            ;
+    }
 }
 
